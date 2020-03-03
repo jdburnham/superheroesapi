b03f1185154a49bd7ec5046042eec0b3d8a0608c
jdburnham <jburnham@stu.southtexascollege.edu>
2020-03-03 14:12:08 -0600

    Lots of Changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <div class="bg-indigo-700 min-h-screen">
+    <NavBar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+}
+</script>
